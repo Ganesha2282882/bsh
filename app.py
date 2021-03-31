a72eac735c9c7197f4467b176a44eff868543a4b
Brahma Sharma <59745725+Ganesha2282882@users.noreply.github.com>
2021-03-31 14:06:10 -0400

    Update app.py

--- app.py
+++ app.py
@@ -25,35 +25,6 @@ while True:
 	elif "exit" == cmd[0]:
 		sys.exit()
 
-	else:# BSH code
-# BSH is a simplistic shell.
-
-import os
-import sys
-import subprocess
-
-def getargs(cmd):
-	output = cmd.split(" ")
-	del output[0]
-	return output
-
-while True:
-	cmd = input("% ")
-	cmd = cmd.split(" ")
-
-	if "echo" == cmd[0]:
-		cmd = " ".join(cmd)
-		print(" ".join(getargs(cmd)))
-
-	elif "cd" == cmd[0]:
-		cmd = " ".join(cmd)
-		os.chdir(" ".join(getargs(cmd)))
-
-	elif "exit" == cmd[0]:
-		sys.exit()
-
 	else:
 		cmd = " ".join(cmd)
-		subprocess.call(cmd.split(" "))
-
 		subprocess.call(cmd.split(" "))
